4628795714de8aba0e89b8c53cf348465ee6427c
MozammelHosen <mdmozammelhosen15701@gmail.com>
2023-01-14 09:29:07 +0600

    mongodb conneted

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000
 
 // Mildware configuration
+app.use(cors());
+app.use(express.json());
+
+// add Mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rq6dngb.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 const users = [
     {id:1, name: 'John', email: 'john@example.com'},
-    {id:1, name: 'amith', email: 'amith@example.com'},
-    {id:1, name: 'smith', email: 'smith@example.com'}
+    {id:2, name: 'amith', email: 'amith@example.com'},
+    {id:3, name: 'smith', email: 'smith@example.com'}
 ]
 app.get("/users", (req, res) => {
     res.send(users)
 })
+// create data post request
+app.post("/users", (req, res) => {
+    console.log("post api called");
+    const user = req.body;
+    user.id= users.length+1;
+    users.push(user)
+    console.log(req.body);
+    res.send(user);
+})
+
 
 app.get('/', (req, res) =>{
     res.send("node server starting")
